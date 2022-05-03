f3e083e522e830d999763f205cbb5cd5c2e27ecd
unknown <mauricio_maletta@hotmail.com>
2022-05-02 23:19:59 -0300

    adicionando progresso de scroll da página

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Image from 'next/image'
 
 import Main from "../Main";
@@ -10,9 +10,30 @@ import Marquee from "../Marquee";
 
 
 const Home: React.FC = () => {
+  const [scrollTop, setScrollTop] = useState<number>(0);
+
+  const onScroll = () => {
+    const winScroll = document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+      const scrolled = (winScroll/height) * 100;
+      setScrollTop(scrolled);
+
+      console.log("scroll number " + scrolled)
+  }
+
+  useEffect(() => {
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+
+  }, [])
+
+
   return (
-    <Container>
-      <MenuBar percent={10}>
+    <Container >
+      <MenuBar percent={scrollTop}>
         <Image src={"/svgs/logo-fiap.svg"} alt="logotipo FIAP" width="64" height="64" />
       </MenuBar>
       <Wrapper>
